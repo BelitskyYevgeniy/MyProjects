fdd76b0dd05fedfb821633292feeb6faba4cd236
BelitskyYevgeniy <78808088+BelitskyYevgeniy@users.noreply.github.com>
2021-04-17 00:25:19 +0300

    adde patnashki to dolder

---
+++ some projects/Patnashki/Patnashki_serialization/Patnashki.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Patnashki_serialization
+{
+    class Patnashki : Button
+    {
+        private int i_;
+        private int j_;
+        public int I
+        {
+            get
+            {
+                return i_;
+            }
+            set
+            {
+                i_ = value;
+            }
+        }
+        public int J
+        {
+            get
+            {
+                return j_;
+
+            }
+            set
+            {
+                j_ = value;
+            }
+        }
+
+
+        public int[,] Table { get; private set; }
+        public bool proverka()
+        {
+            int[] m = new int[15];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 4; j++)
+                    m[i * 4 + j] = Table[i, j];
+            for (int i = 0; i < 3; i++)
+                m[12 + i] = Table[3, i];
+            int sum = 0;
+
+            for (int i = 0; i < 16; i++)
+                for (int j = i + 1; j < 15; j++)
+                    if (m[i] > m[j])
+                        sum++;
+
+            if (sum % 2 == 1)
+                return false;
+            else
+                return true;
+        }
+        public void createButton(Button btn, Form f, int width, int height, string text, int x, int y, EventHandler even, string name, Padding padding)
+        {
+            btn = new Button();
+            btn.Text = text;
+            btn.Left = x;
+            btn.Top = y;
+            btn.Width = width;
+            btn.Height = height;
+            btn.TabStop = false;
+            btn.Name = name;
+            btn.Margin = padding;
+            btn.Click += even;
+            f.Controls.Add(btn);
+        }
+        public void vvod()
+        {
+            System.Collections.Generic.List<int> a;
+            a = new List<int>();
+            for (int i = 1; i < 16; i++)
+                a.Add(i);
+            int k = 15;
+            Random f = new Random();
+            while (a.Count != 0)
+            {
+                int value = f.Next(0, k - 1);
+                if (k % 4 == 0)
+                    Table[k / 4 - 1, 3] = a[value];
+                else
+                    Table[k / 4, k % 4 - 1] = a[value];
+                k--;
+                a.RemoveAt(value);
+            };
+
+            if (Table[3, 3] != 0)
+                for (int i = 0; i < 4; i++)
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (Table[i, j] == 0)
+                        {
+                            if (i != 3 || j != 3)
+                            {
+
+                                Table[i, j] = Table[3, 3];
+                                Table[3, 3] = 0;
+                            }
+                            else
+                                continue;
+                            break;
+                        };
+                    }
+            i_ = 3;
+            j_ = 3;
+        }
+        public bool IsFinished()
+        {
+            int t = 0;
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (t == Table[i, j] - 1)
+                        t++;
+            if (t == 15 && Table[3, 3] == 0)
+                return true;
+            else
+                return false;
+        }
+        public Patnashki()
+        {
+
+            Table = new int[4, 4];
+
+
+            do
+            {
+                vvod();
+            }
+            while (!proverka());
+        }
+    }
+}
+
---
+++ some projects/Patnashki/Patnashki_serialization/Results.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patnashki_serialization
+{
+    [Serializable]
+    class Results : IComparable
+    {
+        private string name;
+        private ulong period;
+        private DateTime startTime;
+        private ulong steps;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public ulong Period
+        {
+            get
+            {
+                return period;
+            }
+        }
+        public DateTime StartTime
+        {
+            get
+            {
+                return startTime;
+            }
+        }
+        public ulong Steps
+        { get
+            {
+                return steps;
+            }
+        }
+        public Results()
+        { }
+        public Results(string Name_, ulong PERIOD, DateTime start,ulong Steps_)
+        {
+            if (Name == "")
+                throw new Exception("Неверный формат ввода: имя не должно быть пустым!");
+            name = Name_;
+            period = PERIOD;
+            startTime = start;
+            steps = Steps_;
+        }
+        public int CompareTo(object obj)
+        {
+            if (period < (obj as Results).Period)
+                return 1;
+            if (period == (obj as Results).Period)
+                return 0;
+            return -1;
+        }
+        
+    }
+    class ComparerResults:IComparer<Results>
+    {
+        public int Compare(Results a, Results b)
+        {
+            if (a.Steps < b.Steps)
+                return 1;
+            if (a.Steps == b.Steps)
+                return 0;
+            return -1;
+        }
+    }
+}
